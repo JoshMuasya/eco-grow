43702b51b6d1eabea2b3190eca04bd2a047b6ae1
JoshMuasya <muasyajoshua07@gmail.com>
2023-09-28 07:31:45 +0300

    Added Coming Soon page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,194 @@
-import Image from 'next/image'
+"use client"
 
-export default function Home() {
+import React, {useState, useEffect} from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PermPhoneMsgIcon from '@mui/icons-material/PermPhoneMsg';
+import EmailIcon from '@mui/icons-material/Email';
+
+const page = () => {
+  const phone = '+254798040353';
+  const email = 'digimaticmarketers@gmail.com';
+
+  const handleNumberClick = () => {
+    window.location.href = `whatsapp://send?phone=${phone}`
+  }
+
+  const handleMailClick = () => {
+    window.location.href = `mailto:${email}`;
+  }
+
+  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());    
+
+  function calculateTimeRemaining() {
+    const targetDate = new Date('2023-10-31T23:59:59').getTime();
+    const now = new Date().getTime();
+    const difference = targetDate - now;
+
+    if (difference <= 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds };
+  }
+
+  useEffect(() => {
+    const isServer = typeof window === 'undefined';
+    if (!isServer) {
+      const countdownInterval = setInterval(() => {
+        setTimeout(() => {
+          const newTimeRemaining = calculateTimeRemaining();
+          setTimeRemaining(newTimeRemaining);
+        }, 1000)
+      }, 1000);
+  
+      return () => clearInterval(countdownInterval);
+    }
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className='h-screen bg-pic bg-fixed bg-cover flex flex-col justify-center align-middle items-center'>
+      {/* overlay */}
+      <div className='absolute top-0 left-0 bottom-0 right-0 h-screen bg-back-green opacity-50 z-[2]'/>
+
+      {/* Top */}
+      <div className='absolute z-10 top-0 w-full flex md:flex-row flex-col justify-between align-middle items-center px-14 pt-5'>
+        {/* Logo */}
+        <div>
+          <Image 
+            src='/Logo.png'
+            width={342}
+            height={162.08}
+            alt='Logo'
+          />
+        </div>
+
+        {/* social links */}
+        <div className='cursor-pointer text-dark-green pt-5 md:pt-0'>
+          <Link
+            href=''
+            className='pr-5'
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <TwitterIcon className='text-xl'/>
+          </Link>
+
+          <Link
+            href=''
+            className='pr-5'
+          >
+            <FacebookIcon className='text-xl'/>
+          </Link>
+          
+          <Link
+            href=''
+          >
+            <InstagramIcon className='text-xl'/>
+          </Link>          
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Bottom */}
+      <div className='absolute z-10 w-full flex flex-col m:flex-row justify-between align-middle items-center px-20 pt-24'>
+        {/* Left */}
+        <div className='text-dark'>
+          {/* Top */}
+          <div className='text-l font-signika font-bold'>
+            OUR WEBSITE IS
+          </div>
+
+          {/* middle */}
+          <div className='text-ml font-black-san font-black'>
+            COMING SOON
+          </div>
+
+          {/* countdown */}
+          <div className='flex flex-row justify-center align-middle items-center'>
+            {/* Days */}
+            <div className='bg-dark-blue text-back-green font-black-san font-bold text-l rounded-xl mx-2'>
+              <h1 className='px-4 py-3'>
+                {timeRemaining.days}
+              </h1>
+            </div>
+
+            {/* Hours */}
+            <div className='bg-dark-blue text-back-green font-black-san font-bold text-l rounded-xl mx-2'>
+              <h1 className='px-4 py-3'>
+                {timeRemaining.hours}
+              </h1>
+            </div>
+
+            {/* Minutes */}
+            <div className='bg-dark-blue text-back-green font-black-san font-bold text-l rounded-xl mx-2'>
+              <h1 className='px-4 py-3'>
+                {timeRemaining.minutes}
+              </h1>
+            </div>
+
+            {/* Seconds */}
+            <div className='bg-dark-blue text-back-green font-black-san font-bold text-l rounded-xl mx-2'>
+              <h1 className='px-4 py-3'>
+                {timeRemaining.seconds}
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        {/* Right */}
+        <div className='bg-dark-blue text-back-green p-5 rounded-2xl flex flex-col justify-center align-middle items-center'>
+          {/* Title */}
+          <div className='font-signika font-bold text-l pb-4'>
+            Contact Us
+          </div>
+
+          {/* Phone */}
+          <div className='flex flex-row justify-center items-center align-middle pb-4'>
+            {/* icon */}
+            <div>
+              <PermPhoneMsgIcon 
+                className='text-ml'
+              />
+            </div>
+
+            {/* Number */}
+            <a 
+              className='font-signika text-lm pl-5 font-semibold cursor-pointer'
+              onClick={handleNumberClick}
+            >
+              {phone}
+            </a>
+          </div>
+
+          {/* email */}
+          <div className='flex flex-row justify-center items-center align-middle pb-4'>
+            {/* icon */}
+            <div>
+              <EmailIcon 
+                className='text-ml'
+              />
+            </div>
+
+            {/* Email */}
+            <a 
+              className='font-signika text-lm pl-5 font-semibold cursor-pointer'
+              onClick={handleMailClick}
+            >
+              {email}
+            </a>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      
+    </div>
   )
 }
+
+export default page
